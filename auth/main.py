1594e446bd43c9e7511ab4b312049bc298e6463f
HanZawNyein <hanzawnyineonline@gmail.com>
2023-02-15 23:13:48 +0630

    remove middleware in auth

--- auth/main.py
+++ auth/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from database.middleware import CustomHeaderMiddleware
 from .routes import auth
 
 app = FastAPI()
 
-# middleware
-app.add_middleware(CustomHeaderMiddleware)
 app.include_router(auth.router)
